980ab70be69faffed5aa4c5191b3cd11a6cbc834
jcarlossc <josecscosta@fac.pe.senac.br>
2025-04-10 10:31:36 -0300

    Classe Calendário adicionada.

---
+++ src/calendario/Calendario.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\calendario;
+
+use DateTime;
+use DateTimeZone;
+
+/**
+ * A classe Calendário é responsável por 
+ * gerar data e hora para o sistema.
+ * 
+ * @author Carlos da Costa
+ * @version 1.0
+ */
+class Calendario {
+    /**
+     * @var DateTime
+     */
+    private DateTime $dataHora;
+
+    /**
+     * Método construtor.
+     * 
+     * @param string $data
+     * @param string $fusoHorario
+     */
+    public function __construct(string $data = "now", string $fusoHorario = "America/Recife") {
+        $this->dataHora = new DateTime($data, new DateTimeZone($fusoHorario));
+    }    
+
+    /**
+     * Método responsável por gerar
+     * data para o sistema.
+     * 
+     * @return string data.
+     */
+    public function getData(): string {
+        return $this->dataHora->format("d/m/Y");
+    }
+
+    /**
+     * Método responsável por gerar
+     * horário para o sistema.
+     * 
+     * @return string
+     */
+    public function getHora(): string {
+        return $this->dataHora->format("H:i:s");
+    }
+}
